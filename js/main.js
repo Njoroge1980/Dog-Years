5848d1dd86eefb3d97447bc7d7108ac9d02af32e
Njoroge Mungai <njorogemungai1980@gmail.com>
2020-07-04 20:55:49 +0300

    add the initial folder structure

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//declare a variable and assign it my age.
+let myAge = 22;//Math.floor(Math.random()*10);
+//declare a variable that changes its value
+let earlyYears = 2;
+ earlyYears *= 10.5;
+//declare a variable that changes value.
+let laterYears = myAge - 2;
+//multiply laterYears by 4 and assign its value in one step.
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+//to sum up the dog years, add earlyYears and laterYears
+let myAgeInDogYears = earlyYears + laterYears;
+//declare a variable and assign it the value of my name
+let myName = 'Njoroge'.toLowerCase()
+//to print out the concatenated values of myName,myAge and myAgeInDogYears.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
+
+
